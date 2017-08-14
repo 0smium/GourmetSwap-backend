5f27962c70f076cc0e65dbe3d1793d79a6550873
0smium <steve.walsh@transfueler.com>
2017-08-14 13:55:59 -0700

    added lib/util.js, added path package, fixed formating in profile-router

--- router/profile-router.js
+++ router/profile-router.js
@@ -1,12 +1,13 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import parserBody from '../lib/parser-body.js';
 import Profile from '../model/profile.js';
-import {bearerAuth} from '../lib/parser-auth.js';
+import { bearerAuth } from '../lib/parser-auth.js';
 
-export default new Router()
-  .post('/api/profiles', bearerAuth, parserBody, (req, res, next) => {
-    console.log('profile req.body: ', req.body);
-    Profile.create(req)
-      .then(res.json)
-      .catch(next);
-  });
+const profileRouter = module.exports = new Router();
+
+profileRouter.post('/api/profiles', bearerAuth, parserBody, (req, res, next) => {
+  console.log('profile req.body: ', req.body);
+  Profile.create(req)
+    .then(res.json)
+    .catch(next);
+});
