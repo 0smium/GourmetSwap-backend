4f89436842e5200a42561810307fb7105eda1699
0smium <steve.walsh@transfueler.com>
2017-08-14 14:03:18 -0700

    disabled parserBody

--- router/profile-router.js
+++ router/profile-router.js
@@ -5,7 +5,8 @@ import { bearerAuth } from '../lib/parser-auth.js';
 
 const profileRouter = module.exports = new Router();
 
-profileRouter.post('/api/profiles', bearerAuth, parserBody, (req, res, next) => {
+// profileRouter.post('/api/profiles', bearerAuth, parserBody, (req, res, next) => {
+profileRouter.post('/api/profiles', bearerAuth, (req, res, next) => {
   console.log('profile req.body: ', req.body);
   Profile.create(req)
     .then(res.json)
