1a6b0990cbe38b38695008a92ead9f19912fc7c7
0smium <steve.walsh@transfueler.com>
2017-08-14 13:30:49 -0700

    changed profile route from /profiles to /api/profiles and loaded the router in server.js

--- router/profile-router.js
+++ router/profile-router.js
@@ -4,7 +4,7 @@ import Profile from '../model/profile.js';
 import {bearerAuth} from '../lib/parser-auth.js';
 
 export default new Router()
-  .post('/profiles', bearerAuth, parserBody, (req, res, next) => {
+  .post('/api/profiles', bearerAuth, parserBody, (req, res, next) => {
     console.log('profile req.body: ', req.body);
     Profile.create(req)
       .then(res.json)
