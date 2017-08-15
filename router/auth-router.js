3d9dc6b502743e5e2525bacd3307c0308abe0b98
Justin Green <justin98366@gmail.com>
2017-08-14 17:03:19 -0700

    can now upday email for auth

--- router/auth-router.js
+++ router/auth-router.js
@@ -4,6 +4,8 @@ const jsonParser = require('body-parser').json();
 const { Router } = require('express');
 const basicAuth = require('../lib/basic-auth-middleware.js');
 const User = require('../model/user.js');
+import { bearerAuth } from '../lib/parser-auth.js';
+import parserBody from '../lib/parser-body.js';
 
 const authRouter = module.exports = new Router();
 
@@ -20,3 +22,25 @@ authRouter.post('/api/signup', jsonParser, (req, res, next) => {
 authRouter.get('/api/signin', basicAuth, (req, res, next) => {
   req.user.tokenCreate().then(token => res.send(token)).catch(next);
 });
+
+authRouter.put('/api/users', bearerAuth, parserBody, (req, res, next) => {
+  let options = {
+    new: true,
+    runValidators: true,
+  };
+  
+  User.findOneAndUpdate(req.user._id, req.body, options)
+    .then(profile => res.status(202).json(profile))
+    .catch(next);
+});
+
+// authRouter.put('/api/passwords', basicAuth, parserBody, (req, res, next) => {
+//   let options = {
+//     new: true,
+//     runValidators: true,
+//   };
+  
+//   User.findOneAndUpdate(req.user._id, req.body, options)
+//     .then(profile => res.status(202).json(profile))
+//     .catch(next);
+// });
